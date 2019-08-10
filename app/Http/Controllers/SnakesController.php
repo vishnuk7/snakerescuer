a41bfa419d9efe8a288942fa63e24cc341c5b7c6
unknown <shuklaanupam18@gmail.com>
2019-08-10 12:54:47 +0530

    Changed filename format

--- app/Http/Controllers/SnakesController.php
+++ app/Http/Controllers/SnakesController.php
@@ -14,20 +14,24 @@ class SnakesController extends Controller
         $snake = new Snake();
         if($request->hasFile('file')){
         $filename = $request->file->getClientOriginalName();
+        $filename = date('dmYhis').$filename;
 
-        $snake->image = date('Y-m-d-H:i:s').$filename;
-        $snake->species=request('species');
-        $snake->description=request('desc');
-        $snake->date=request('date');
-        $snake->time=request('time');
-        $snake->location=request('location');
+        $snake->image = $filename;
+        $snake->species = request('species');
+        $snake->description = request('desc');
+        $snake->date = request('date');
+        $snake->time = request('time');
+        $snake->location = request('location');
         $snake->save();
+
 
         $path = public_path('storage/upload/snake/' . $filename);
         Image::make($request->file('file')->getRealPath())->resize(300, 200)->save($path);
-            // send mail
-        // Mail::Send(new snakeDetails());
-        }
+
+        // send mail
+        Mail::Send(new snakeDetails());
+
+    }
         return redirect('/');
     }
 }
