06bf1bdcd27b67a2fead4455e89cfbf7b12e53db
unknown <shuklaanupam18@gmail.com>
2019-08-10 10:34:23 +0530

    Fixed issue #12

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -29,12 +29,12 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+            aria-controls="collapseOne">
             <i class="fas fa-fw fa-cog"></i>
             <span>Rescuer</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Actions:</h6>
                 <a class="collapse-item" href="/admin/add-rescuer">Add rescuer</a>
@@ -53,7 +53,7 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Actions:</h6>
-                <a class="collapse-item" href="cards.html">View rescuers</a>
+                <a class="collapse-item" href="cards.html">Rescued snakes</a>
             </div>
         </div>
     </li>
