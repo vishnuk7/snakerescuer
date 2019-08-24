7a7cf500b021113d0c01c222cf5653665af719d2
Anupam Shukla <35293695+anupm12@users.noreply.github.com>
2019-08-24 15:37:36 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::post('/admin/rescuer', 'RescuersController@store');
 Route::get('/admin/rescued-snakes','SnakesController@viewSnakes')->name('rescued-snakes');
 
 // delete snake
-Route::get('/admin/rescued-snakes/delete/{id}', ['uses' => 'SnakesController@destroy', 'as' => 'snakes.delete']);
+Route::get('/admin/rescued-snakes/delete/{id}/{image}', ['uses' => 'SnakesController@destroy', 'as' => 'snakes.delete']);
 
 
 //(admin) view all user
