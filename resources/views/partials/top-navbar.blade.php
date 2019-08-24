66920133f703959151f9b0e32e5b17d28164e6c1
vishnuk7 <vishnu.kraman88@gmail.com>
2019-08-24 19:18:37 +0530

    Style dropdown #40

--- resources/views/partials/top-navbar.blade.php
+++ resources/views/partials/top-navbar.blade.php
@@ -26,7 +26,7 @@
       </li>
       @else
       <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            <a href="#" class="nav-link nav-link-custom dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
              {{ Auth::user()->name }} <span class="caret"></span>
             </a>
             <ul class="dropdown-menu" role="menu">
